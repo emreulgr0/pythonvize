f285460adcd299b80cca4fe21cbf31979e6473ed
emreulgr0 <94914897+emreulgr0@users.noreply.github.com>
2021-12-06 01:16:54 +0300

    Add files via upload

---
+++ pythonyemekuyg.py
@@ -0,0 +1,14 @@
+
+
+print(" -- RECEP KITCHEN -- ")
+
+
+
+yemekkayıt = open("yemektarifleri.txt", "w") 
+yemekkayıt.write(input("Yemek Adı Giriniz = " )) 
+yemekkayıt.write(input("Yemek Tarifi Giriniz = " ))
+yemekkayıt.close()
+
+dosya = open("yemektarifleri.txt","r")
+oku = dosya.read()
+print("Yemek Adı ve Tarifiniz = " ,oku)
